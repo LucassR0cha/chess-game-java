85bd37cec56a22ae42c03c032d70b245c9f2abcb
LucasRocha <lucascaixeta98@gmail.com>
2023-10-07 15:34:07 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -44,6 +44,9 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("Nao existe peca na posicao de origim");
 		}
+		if (!board.piece(position).IsThereAnyPossibleMove()) {
+			throw new ChessException("Nao existe movimentos possiveis para a peca escolhida");
+		}
 	}
 
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
